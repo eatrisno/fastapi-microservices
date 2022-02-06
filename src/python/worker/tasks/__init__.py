3eb9c71bc9e26908d6771068a48d2b2aa753127d
Eko Aprili Trisno <eaprilitrisno@gmail.com>
2022-02-07 03:19:57 +0700

    fix typo

---
+++ src/python/worker/tasks/__init__.py
@@ -0,0 +1,2 @@
+from worker.tasks.hello import hello_task
+from worker.tasks.test import test_task
---
+++ src/python/worker/tasks/test.py
@@ -0,0 +1,5 @@
+import asyncio
+
+async def test_task(ctx: dict, word: str) -> str:
+    await asyncio.sleep(10)
+    return f"test task return {word}"
